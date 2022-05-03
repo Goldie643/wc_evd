bb884132bf91ca450fd26a7a47f91196b08d4766
Alex Goldsack <alex.goldsack1@gmail.com>
2022-05-03 15:12:41 +0100

    Initial commit

---
+++ event_display.js
@@ -0,0 +1,32 @@
+// Event Display Code
+
+// Some hard coded detector geometry (to be later sent via the server).
+const detector = [];
+detector.push( new THREE.Vector3( - 10, 0, 0 ) );
+detector.push( new THREE.Vector3( 0, 10, 0 ) );
+detector.push( new THREE.Vector3( 10, 0, 0 ) );
+
+// Use and convert detector geo into an actual mesh.
+function PlotDetector(detector) {
+    const geometry = new THREE.BufferGeometry().setFromPoints(detector);
+    const material = new THREE.LineBasicMaterial( { color: 0x0000ff } );
+    const mesh = new THREE.Line(geometry, material);
+
+    return mesh;
+}
+
+// Setup a default camera (other control types like Ortho are available).
+const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 500 );
+camera.position.set( 0, 0, 100 );
+camera.lookAt( 0, 0, 0 );
+
+// Setup a scene, where shit lives, including add the detector
+const scene = new THREE.Scene();
+const detectorMesh = PlotDetector(detector)
+scene.add(detectorMesh);
+
+// Now actually start to render stuff
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+renderer.render(scene, camera);
