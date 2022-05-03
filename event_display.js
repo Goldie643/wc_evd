e2ed7cd16238095803e382b97aa4e578738797b0
goldie643 <alex.goldsack1@gmail.com>
2022-05-04 00:43:10 +0100

    Display transparent cylinder with orbit camera.

--- event_display.js
+++ event_display.js
@@ -1,3 +1,7 @@
+import * as THREE from "three"
+
+import { OrbitControls } from "./OrbitControls.js"
+
 // Event Display Code
 
 // Some hard coded detector geometry (to be later sent via the server).
@@ -8,8 +12,8 @@ detector.push( new THREE.Vector3( 10, 0, 0 ) );
 
 // Use and convert detector geo into an actual mesh.
 function PlotDetector(detector) {
-    const geometry = new THREE.CylinderGeometry( 5, 5, 20, 32 );
-    // const geometry = new THREE.CylinderGeometry( 1690, 1690, 2*1810, 64);
+    // const geometry = new THREE.CylinderGeometry( 5, 5, 20, 32 );
+    const geometry = new THREE.CylinderGeometry( 1690, 1690, 2*1810, 64);
     const material = new THREE.MeshBasicMaterial( {color: 0x808080, transparent:
         true, opacity: 0.5} );
     const mesh = new THREE.Mesh( geometry, material );
@@ -21,25 +25,31 @@ function PlotDetector(detector) {
     return mesh;
 }
 
-// Setup a default camera (other control types like Ortho are available).
-const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 500 );
-// const controls = new OrbitControls( camera, renderer.domElement );
-camera.position.set( 0, 0, 100 );
-// controls.update()
-camera.lookAt( 0, 0, 0 );
-
-// Setup a scene, where shit lives, including add the detector
-const scene = new THREE.Scene();
-const detectorMesh = PlotDetector(detector);
-scene.add(detectorMesh);
-
-// Now actually start to render stuff
+// Setup renderer
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
+// Setup a scene
+const scene = new THREE.Scene();
+const detectorMesh = PlotDetector(detector);
+scene.add(detectorMesh);
+
+// Setup a default camera (other control types like Ortho are available).
+const camera = new THREE.PerspectiveCamera( 45, window.innerWidth /
+    window.innerHeight, 1, 100000 );
+
+// Control camera with orbit controls
+const controls = new OrbitControls( camera, renderer.domElement );
+
+camera.position.set( 0, 5000, 10000 );
+camera.lookAt( 0, 0, 0 );
+
+controls.update()
+
 // renderer.render( scene, camera );
 function animate() {
+    controls.update()
 	requestAnimationFrame( animate );
 	renderer.render( scene, camera );
 };
