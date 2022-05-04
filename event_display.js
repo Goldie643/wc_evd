cb3aeec00731a86f403413b9d8d9e4f65d2a0315
goldie643 <alex.goldsack1@gmail.com>
2022-05-04 01:02:29 +0100

    Starting PMT plotting code.

--- event_display.js
+++ event_display.js
@@ -5,14 +5,16 @@ import { OrbitControls } from "./OrbitControls.js"
 // Event Display Code
 
 // Some hard coded detector geometry (to be later sent via the server).
-const detector = [];
-detector.push( new THREE.Vector3( - 10, 0, 0 ) );
-detector.push( new THREE.Vector3( 0, 10, 0 ) );
-detector.push( new THREE.Vector3( 10, 0, 0 ) );
+// const detector = [];
+// detector.push( new THREE.Vector3( - 10, 0, 0 ) );
+// detector.push( new THREE.Vector3( 0, 10, 0 ) );
+// detector.push( new THREE.Vector3( 10, 0, 0 ) );
+
+const hits = [];
+hits.push( new THREE.Vector3( 0, 0, 0 ) );
 
 // Use and convert detector geo into an actual mesh.
-function PlotDetector(detector) {
-    // const geometry = new THREE.CylinderGeometry( 5, 5, 20, 32 );
+function PlotDetector() {
     const geometry = new THREE.CylinderGeometry( 1690, 1690, 2*1810, 64);
     const material = new THREE.MeshBasicMaterial( {color: 0x808080, transparent:
         true, opacity: 0.5} );
@@ -25,6 +27,17 @@ function PlotDetector(detector) {
     return mesh;
 }
 
+function PlotHits( scene, hits ) {
+    const material = new THREE.MeshBasicMaterial( {color: 0xFFFF00} );
+    const hit_geom = new THREE.SphereGeometry( 25, 32, 16 );
+    for (let hit of hits) {
+        const mesh = new THREE.Mesh( hit_geom, material )
+        // mesh.position.set( hit );
+        scene.add( mesh );
+    }
+
+}
+
 // Setup renderer
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
@@ -32,8 +45,10 @@ document.body.appendChild(renderer.domElement);
 
 // Setup a scene
 const scene = new THREE.Scene();
-const detectorMesh = PlotDetector(detector);
+const detectorMesh = PlotDetector();
 scene.add(detectorMesh);
+
+PlotHits( scene, hits );
 
 // Setup a default camera (other control types like Ortho are available).
 const camera = new THREE.PerspectiveCamera( 45, window.innerWidth /
