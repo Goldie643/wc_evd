d5d97320b4d4fc95e98ecb2c9ee67bdebfa71953
Alex Goldsack <alex.goldsack1@gmail.com>
2022-05-04 14:29:41 +0100

    Start adding PMT info parsing.

--- event_display.js
+++ event_display.js
@@ -2,6 +2,9 @@ import * as THREE from "three"
 
 import { OrbitControls } from "./OrbitControls.js"
 
+const fs = require('fs');
+
+
 // Event Display Code
 
 // Some hard coded detector geometry (to be later sent via the server).
@@ -13,11 +16,33 @@ import { OrbitControls } from "./OrbitControls.js"
 const hits = [];
 hits.push( new THREE.Vector3( 1690, 1810, 0 ) );
 
+// Parses the PMT position/production date info from the .dat file.
+function ParsePMTInfo() {
+    let text = fs.readFileSync('pmt_prod_year.dat', 'utf8');
+
+    const pmt_info = [];
+
+    for (let line of text.trim().split('\n')) {
+
+        let s = line.trim().split(/[#\s]+/g);
+
+        pmt_info.push({
+            id: s[0],
+            prod_month: s[1],
+            x: s[2],
+            y: s[3],
+            z: s[4],
+        });
+    }
+
+    return pmt_info
+}
+
 // Use and convert detector geo into an actual mesh.
 function PlotDetector() {
     const geometry = new THREE.CylinderGeometry( 1690, 1690, 2*1810, 64);
-    const material = new THREE.MeshBasicMaterial( {color: 0x808080, transparent:
-        true, opacity: 0.5} );
+    const material = new THREE.MeshBasicMaterial( {color: 0x808080,
+        transparent: true, opacity: 0.5} );
     const mesh = new THREE.Mesh( geometry, material );
 
     // const geometry = new THREE.BufferGeometry().setFromPoints(detector);
@@ -27,16 +52,29 @@ function PlotDetector() {
     return mesh;
 }
 
+// Plot yellow spheres for a list of Vector3s
 function PlotHits( scene, hits ) {
-    const material = new THREE.MeshBasicMaterial( {color: 0xFFFF00} );
     const hit_geom = new THREE.SphereGeometry( 25, 32, 16 );
+    const material = new THREE.MeshBasicMaterial( {color: 0xFFFF00} );
     for (let hit of hits) {
         const mesh = new THREE.Mesh( hit_geom, material )
         mesh.position.copy( hit );
-        // mesh.position.set( 100, 100, 100 );
         scene.add( mesh );
     }
 
+    return
+}
+
+// Plot all PMTs from pmt_info
+function PlotPMTs( scene, pmt_info ) {
+    const pmt_geom = new THREE.SphereGeometry( 25, 32, 16 );
+    const material = new THREE.MeshBasicMaterial( {color: 0x808080,
+        transparent: true, opacity: 0.5} );
+    for (let pmt of pmt_info) {
+        const mesh = new THREE.Mesh( pmt_geom, material );
+        mesh.position.set( pmt.x, pmt.y, pmt.z );
+        scene.add( mesh );
+    }
 }
 
 // Setup renderer
@@ -49,7 +87,10 @@ const scene = new THREE.Scene();
 const detectorMesh = PlotDetector();
 scene.add(detectorMesh);
 
+pmt_info = ParsePMTInfo()
+
 PlotHits( scene, hits );
+PlotPMTs( scene, pmt_info );
 
 // Setup a default camera (other control types like Ortho are available).
 const camera = new THREE.PerspectiveCamera( 45, window.innerWidth /
